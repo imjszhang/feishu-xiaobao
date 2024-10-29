bb87bbe7c2335a085f88887ba09ac51a39b0fd93
imjszhang <ortle3x3@gmail.com>
2024-10-29 23:59:02 +0800

    Refactor default server host in run_server method

--- api/api.py
+++ api/api.py
@@ -26,7 +26,7 @@ class API:
         self.app.include_router(test.router)
         self.app.include_router(scraper.router)
 
-    def run_server(self, host="127.0.0.1", port=8000, log_level="info"):
+    def run_server(self, host="0.0.0.0", port=8000, log_level="info"):
         config = uvicorn.Config(self.app, host=host, port=port, log_level=log_level)
         server = uvicorn.Server(config)
         server.run()
